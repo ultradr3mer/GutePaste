2a4bd361525c1b3a9ed629946dd5a12b11f0c8c5
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2022-06-16 11:38:43 +0200

    initial commit

---
+++ GutePaste/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GutePaste.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
